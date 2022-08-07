4c3d7b0a8ada7af80a9d9bd77b838512d5fb42bb
Mohd Danish <formohddanish@Mohds-MacBook-Air.local>
2022-08-07 08:31:32 +0530

    started project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {useEffect, useState} from 'react'
+
+function App() {
+  
+  
+  return (
+    <h1>Hello</h1>
+
+  );
+}
+
+export default App;
