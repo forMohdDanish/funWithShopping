b850905e73ab6795a671a025cc08a043f80d8a14
Mohd Danish <formohddanish@Mohds-MacBook-Air.local>
2022-08-07 12:44:30 +0530

    completed app with synchronous api call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
+import Header from "./containers/Header";
+import {BrowserRouter,Switch,Route} from "react-router-dom"
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 
 function App() {
   return (
-    <div>
-      Hello
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 Not Found</Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/containers/ProductListing.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import ProductComponent from './ProductComponent';
+import axios from "axios";
+import { setProducts } from '../redux/actions/productAction';
+
+function ProductListing() {
+    const products=useSelector(state=>state);
+    const dispatch=useDispatch();
+    const fetchProducts=async()=>{
+        const {data}=await axios.get('https://fakestoreapi.com/products').catch(err=>console.log("err",err))
+        console.log(data);
+        dispatch(setProducts(data))
+    }
+    useEffect(() => {
+      fetchProducts();
+    }, []);
+    console.log(products, "hey");
+    
+  return (
+    
+    <div className="ui grid container">
+       <ProductComponent/>
+    </div>
+  )
+}
+
+export default ProductListing
